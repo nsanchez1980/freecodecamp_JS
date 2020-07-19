86111a56f45a4a3c4bc3be0cbdefa9aab5241d58
nsanchez1980 <67709250+nsanchez1980@users.noreply.github.com>
2020-07-19 11:39:19 -0300

    Revert "Revert "Basic Algorithm Scripting - JS - freecodecamp""
    This reverts commit c8f8de96252e24c08b76549699da26535752d582.

---
+++ freecodecamp javascript course/BAS_mutations.js
@@ -0,0 +1,12 @@
+function mutation(arr) {
+    
+    for (let l of arr[1].toLowerCase()){
+        if (arr[0].toLowerCase().search(l)==-1){
+            return false;
+        }
+    }
+    return true;
+  }
+  
+  console.log(mutation(["hello", "hey"]));
+  
